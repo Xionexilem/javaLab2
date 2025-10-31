b7fc341b5ff507fafa69dcf6af4ef7ef86bb3f91
Xionexilem <lev.mitrakov@bk.ru>
2025-10-31 18:07:43 +0500

    complete task 2

---
+++ src/Department.java
@@ -0,0 +1,25 @@
+public class Department {
+  private final String name;
+  private Employee boss;
+
+  public Department(String name) {
+    this.name = name;
+  }
+
+  public Employee getBoss() {
+    return this.boss;
+  }
+
+  public void setBoss(Employee boss) {
+    if (boss.getDepartment() == this) {
+      this.boss = boss;
+    } else {
+      throw new IllegalArgumentException(boss + " does not work in task2.Department " + this);
+    }
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
+}
---
+++ src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+  private final String name;
+  private Department department;
+
+  public Employee(String name, Department department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  public Department getDepartment() {
+    return this.department;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void makeBoss() {
+    this.department.setBoss(this);
+  }
+
+  @Override
+  public String toString() {
+    if (this == this.department.getBoss()) {
+      return this.name + " начальник отдела " + this.department;
+    } else {
+      return this.name + " работает в отделе " + this.department + ", начальник которого " + this.department.getBoss().getName();
+    }
+  }
+}
